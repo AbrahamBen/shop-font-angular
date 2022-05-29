1921e282dd8f3e49dfbb379609861d9ea399bdfc
AbrahamBen <eternelle.vision@gmail.com>
2022-05-29 15:44:01 +0200

    Mise en place du projet 1

---
+++ src/app/components/partials/modals/add-to-card-modal/add-to-card-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'node-add-to-card-modal',
+  templateUrl: './add-to-card-modal.component.html',
+  styleUrls: ['./add-to-card-modal.component.css']
+})
+export class AddToCardModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
